f658b2312fe681325956cc89b278596da03e0716
Matt Stine <matt.stine@gmail.com>
2019-05-15 14:56:59 -0500

    [LAB 7] submission

--- src/main/java/com/mattstine/dddworkshop/pizzashop/delivery/InProcessEventSourcedDeliveryOrderRepository.java
+++ src/main/java/com/mattstine/dddworkshop/pizzashop/delivery/InProcessEventSourcedDeliveryOrderRepository.java
@@ -5,10 +5,14 @@ import com.mattstine.dddworkshop.pizzashop.infrastructure.events.ports.Topic;
 import com.mattstine.dddworkshop.pizzashop.infrastructure.repository.adapters.InProcessEventSourcedRepository;
 import com.mattstine.dddworkshop.pizzashop.kitchen.KitchenOrderRef;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Matt Stine
  */
 final class InProcessEventSourcedDeliveryOrderRepository extends InProcessEventSourcedRepository<DeliveryOrderRef, DeliveryOrder, DeliveryOrder.OrderState, DeliveryOrderEvent, DeliveryOrderAddedEvent> implements DeliveryOrderRepository {
+	Map<KitchenOrderRef, DeliveryOrderRef> kitchenOrderRefToDeliveryOrderRef;
 
 	InProcessEventSourcedDeliveryOrderRepository(EventLog eventLog, Topic topic) {
 		super(eventLog,
@@ -17,10 +21,23 @@ final class InProcessEventSourcedDeliveryOrderRepository extends InProcessEventS
 				DeliveryOrder.OrderState.class,
 				DeliveryOrderAddedEvent.class,
 				topic);
+
+		kitchenOrderRefToDeliveryOrderRef = new HashMap<>();
+
+		eventLog.subscribe(new Topic("delivery_orders"), e -> {
+			if (e instanceof DeliveryOrderAddedEvent) {
+				DeliveryOrderAddedEvent doae = (DeliveryOrderAddedEvent) e;
+				kitchenOrderRefToDeliveryOrderRef.put(doae.getState().getKitchenOrderRef(), doae.getRef());
+			}
+		});
 	}
 
 	@Override
 	public DeliveryOrder findByKitchenOrderRef(KitchenOrderRef kitchenOrderRef) {
+		DeliveryOrderRef deliveryOrderRef = kitchenOrderRefToDeliveryOrderRef.get(kitchenOrderRef);
+		if (deliveryOrderRef != null) {
+			return findByRef(deliveryOrderRef);
+		}
 		return null;
 	}
 }
